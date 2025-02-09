25cac8cfb95fe175bb4dd577eba97b82b8eb93ae
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2025-02-08 19:59:59 -0700

    added function to start game button

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -9,57 +9,25 @@ export class MainMenu extends Scene {
     }
 
     create() {
-        // this.add.image(1024, 384, 'background');
-        // New
         this.background = this.add.image(0, 0, 'background')
             .setOrigin(0, 0)
             .setDisplaySize(this.cameras.main.width, this.cameras.main.height);
 
         this.logo = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 4, 'logo').setDepth(100);
 
-        this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, 'Main Menu', {
+        this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, 'Start Game', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         })
-            .setDepth(100).setOrigin(0.5);
+            .setDepth(100).setOrigin(0.5).setInteractive().on('pointerdown', () => this.changeScene())
 
 
         EventBus.emit('current-scene-ready', this);
     }
 
     changeScene() {
-        // if (this.logoTween) {
-        //     this.logoTween.stop();
-        //     this.logoTween = null;
-        // }
 
         this.scene.start('Game');
     }
-
-    // moveLogo(vueCallback) {
-    //     if (this.logoTween) {
-    //         if (this.logoTween.isPlaying()) {
-    //             this.logoTween.pause();
-    //         }
-    //         else {
-    //             this.logoTween.play();
-    //         }
-    //     }
-    //     else {
-    //         this.logoTween = this.tweens.add({
-    //             targets: this.logo,
-    //             x: { value: 750, duration: 3000, ease: 'Back.easeInOut' },
-    //             y: { value: 80, duration: 1500, ease: 'Sine.easeOut' },
-    //             yoyo: true,
-    //             repeat: -1,
-    //             onUpdate: () => {
-    //                 vueCallback({
-    //                     x: Math.floor(this.logo.x),
-    //                     y: Math.floor(this.logo.y)
-    //                 });
-    //             }
-    //         });
-    //     }
-    // }
 }
